e8ec53b1ad0df77a5a1f38d6a4d2e4342d17d56b
ampir-nnn <ampirnnn@gmail.com>
2019-07-02 15:27:53 +0000

    adding my files to my repository

---
+++ aosp_c106.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit device configuration
+$(call inherit-product, device/coolpad/c106/full_c106.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := c106
+PRODUCT_NAME := aosp_c106
+PRODUCT_BRAND := LeEco
+PRODUCT_MANUFACTURER := LeMobile
+
+PRODUCT_GMS_CLIENTID_BASE := android-leeco
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=le_c106 \
+    PRODUCT_NAME=c106_CN \
+    PRIVATE_BUILD_DESC="c106-user 6.0.1 IIXOSOP5801910121S 44 release-keys"
+
+BUILD_FINGERPRINT := Letv/c106_WW/le_c106_ww:6.0.1/IIXOSOP5801910121S/44:user/release-keys
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
+
+# Release name
+PRODUCT_RELEASE_NAME := c106
