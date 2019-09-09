c7cde65ced9c6a7caaa5ccdc7b932b3644fbfd84
epicX67 <46811948+epicX67@users.noreply.github.com>
2019-09-09 16:49:31 +0530

    Update shrp_env.sh

--- shrp_env.sh
+++ shrp_env.sh
@@ -1,19 +1,19 @@
 # !/bin/bash
 
-export SHRP_MAINTAINER_NAME=epicX
-export SHRP_PRODUCT=c103
-export SHRP_EDL="1"
-export SHRP_EXTERNAL_MOUNT=/external_sd
-export SHRP_INTERNAL_MOUNT=/sdcard
-export SHRP_OTG_MOUNT=/usb-otg
-export SHRP_FLASHLIGHT=1
-export SHRP_FLASH_ON_PATH_1=echo 200 > /sys/class/leds/led:torch_0/brightness
-export SHRP_FLASH_ON_PATH_2=echo 200 > /sys/class/leds/led:torch_1/brightness
-export SHRP_FLASH_ON_PATH_3=echo 1 > /sys/class/leds/led:switch/brightness
-export SHRP_FLASH_OFF_PATH_1=echo 0 > /sys/class/leds/led:torch_0/brightness
-export SHRP_FLASH_OFF_PATH_2=echo 0 > /sys/class/leds/led:torch_1/brightness
-export SHRP_FLASH_OFF_PATH_3=echo 0 > /sys/class/leds/led:switch/brightness
-export SHRP_RECOVERY_PATH=/dev/block/bootdevice/by-name/recovery
+export SHRP_MAINTAINER="epicX"
+export SHRP_DEVICE="c103"
+export SHRP_EDL_MODE="1"
+export SHRP_EXTERNAL="/external_sd"
+export SHRP_INTERNAL="/sdcard"
+export SHRP_OTG="/usb-otg"
+export SHRP_FLASH="1"
+export SHRP_FONP_1="echo 200 > /sys/class/leds/led:torch_0/brightness"
+export SHRP_FONP_2="echo 200 > /sys/class/leds/led:torch_1/brightness"
+export SHRP_FONP_3="echo 1 > /sys/class/leds/led:switch/brightness"
+export SHRP_FOFP_1="echo 0 > /sys/class/leds/led:torch_0/brightness"
+export SHRP_FOFP_2="echo 0 > /sys/class/leds/led:torch_1/brightness"
+export SHRP_FOFP_3="echo 0 > /sys/class/leds/led:switch/brightness"
+export SHRP_REC="/dev/block/bootdevice/by-name/recovery"
 
 
 source ~/twrp/build/shrp/shrp.sh
