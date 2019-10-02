fe8c61d975191327bfaff92f0ca21df7d59a967c
smustfa147 <smustfa147@gmail.com>
2019-10-02 16:48:24 +0000

    C106: Import Rootdir

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,33 +54,9 @@ include $(BUILD_PREBUILT)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.qcom.early_boot.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.qcom.early_boot.sh
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.post_boot.sh
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       	:= init.safailnet.rc
-LOCAL_MODULE_TAGS  	:= optional eng
-LOCAL_MODULE_CLASS 	:= ETC
-LOCAL_SRC_FILES    	:= etc/init.safailnet.rc
-LOCAL_MODULE_PATH  	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.proc_touchpanel.sh
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.proc_touchpanel.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
