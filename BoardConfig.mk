bcd36041f5d5151b19e4a26e6e26bd0412191b30
epicX67 <46811948+epicX67@users.noreply.github.com>
2019-11-21 10:00:48 +0530

    Fixed A/B Conflicts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,28 +89,15 @@ ALLOW_MISSING_DEPENDENCIES=true
 
 #SHRP_Variables
 SHRP_PATH := device/coolpad/c103
-
-# Maintainer name
 SHRP_MAINTAINER := epicX
-# Device codename
 SHRP_DEVICE_CODE := c103
-# put this 0 if device has no EDL mode
 SHRP_EDL_MODE := 1
 SHRP_EXTERNAL := /external_sd
 SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usb_otg
-# Put 0 to disable flashlight
 SHRP_FLASH := 1
-# These are led paths, find yours then put here
 SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
 SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
 SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
-# Max Brightness of LED
 SHRP_FLASH_MAX_BRIGHTNESS := 200
-# Check your device's recovery path, dont use blindly
 SHRP_REC := /dev/block/bootdevice/by-name/recovery
-# Put true if your device A/B
-SHRP_AB := false
-
-
-
